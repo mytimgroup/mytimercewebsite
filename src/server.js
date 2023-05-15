1fd5029330c7b614713c4142385673ed539ec08b
Mytim Group <mytimgroup@gmail.com>
2023-05-14 20:21:35 -0400

    added api link

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://ecommerce-market-project.herokuapp.com/api/v2";
+export const server = "https://mytimercewebsite.herokuapp.com/api/v2";
 
-export const backend_url = "https://ecommerce-market-project.herokuapp.com/";
+export const backend_url = "https://mytimercewebsite.herokuapp.com/";
